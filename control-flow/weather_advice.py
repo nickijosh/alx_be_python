85673b72f756e9949219c8f6575e0f3c81270100
Nicki Josh <nicholasjoshua26@gmail.com>
2025-05-29 10:49:43 +0100

    Updated the weather_advice.py with more statements

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,12 @@
-# weather_advice.py
+# Prompt User for Weather Input
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-# prompt user for the current weather
-weather = input("What's the weather like today? (sunnu/rainy/cold): ").lower()
-
-# provide clothing recommendation based on input
+# Provide Clothing Recommendations Based on Input
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print("Don't forgrt your umbreall and a raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-else: 
-    print("Sorry, I don't have recommendation for this weather.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
