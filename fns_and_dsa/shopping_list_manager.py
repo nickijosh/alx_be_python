93261fc749b745465ee5874ccdd29c8e15cdb8f5
Nicki Josh <nicholasjoshua26@gmail.com>
2025-06-03 08:11:42 +0100

    Added shopping list manager script

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 # This function displays the options menu to the user
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
